3d080a9eef113dff9a0e1be8302cbdcacd0be415
darshilshahbca <darshil.saphcm@gmail.com>
2020-05-04 02:00:34 +0530

    - Layout added to display Orderstatus - Field added Request Status - Modified Home file for easy work Drawer Toogle - Code added for Cart/Order/Signout

--- app/src/main/java/com/example/androideatit/Home.java
+++ app/src/main/java/com/example/androideatit/Home.java
@@ -13,11 +13,14 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -40,10 +43,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
 
     FirebaseDatabase database;
     DatabaseReference category;
@@ -51,6 +52,10 @@ public class Home extends AppCompatActivity
     TextView txtFullName;
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
 
     @Override
@@ -76,18 +81,39 @@ public class Home extends AppCompatActivity
                startActivity(cartIntent);
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.nav_menu)
+                {
+                    //Handle
+                }
+                else if(menuItem.getItemId() == R.id.nav_cart)
+                {
+                    Intent cartIntent = new Intent(Home.this, Cart.class);
+                    startActivity(cartIntent);
+                }
+                else if(menuItem.getItemId() == R.id.nav_orders){
+                    Intent orderIntent = new Intent(Home.this, OrderStatus.class);
+                    startActivity(orderIntent);
+                }  else if(menuItem.getItemId() == R.id.nav_log_out){
+                    Intent signIn = new Intent(Home.this, SignIn.class);
+                    signIn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(signIn);
+                }
+                return true;
+            }
+        });
+
 
         //Set Name for User
         View headerView = navigationView.getHeaderView(0);
@@ -149,26 +175,12 @@ public class Home extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
 }
---
+++ app/src/main/java/com/example/androideatit/OrderStatus.java
@@ -0,0 +1,91 @@
+package com.example.androideatit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androideatit.Common.Common;
+import com.example.androideatit.Interface.ItemClickListener;
+import com.example.androideatit.Model.Food;
+import com.example.androideatit.Model.Request;
+import com.example.androideatit.ViewHolder.FoodViewHolder;
+import com.example.androideatit.ViewHolder.OrderViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class OrderStatus extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference requests;
+
+    FirebaseRecyclerAdapter<Request, OrderViewHolder> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_status);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        requests = database.getReference("Requests");
+
+        recyclerView = (RecyclerView)findViewById(R.id.listOrders);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        loadOrders(Common.currentUser.getPhone());
+    }
+
+    private void loadOrders(String phone) {
+        FirebaseRecyclerOptions<Request> options =
+                new FirebaseRecyclerOptions.Builder<Request>()
+                        .setQuery(requests.orderByChild("phone").equalTo(phone), Request.class)
+                        .build();
+
+
+        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull OrderViewHolder viewHolder, int position, @NonNull Request model) {
+                viewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
+                viewHolder.txtOrderStatus.setText(convertCodeToStatus(model.getStatus()));
+                viewHolder.txtOrderAddress.setText(model.getAddress());
+                viewHolder.txtOrderPhone.setText(model.getPhone());
+            }
+
+
+            @NonNull
+            @Override
+            public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_layout, parent, false);
+                return new OrderViewHolder(view);
+            }
+        };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private String convertCodeToStatus(String status) {
+        if(status.equals("0"))
+            return "Placed";
+        else if(status.equals("1"))
+            return "On my way";
+        else
+            return "Shipped";
+    }
+
+
+}
