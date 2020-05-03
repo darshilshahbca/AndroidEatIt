381ab2392795a84d34daf1d4398ba06eb020a2d6
darshilshahbca <darshil.saphcm@gmail.com>
2020-05-03 11:55:54 +0530

    -Food Detail Designed Layout -Code added to fetch Food Details from Food table

--- app/src/main/java/com/example/androideatit/FoodList.java
+++ app/src/main/java/com/example/androideatit/FoodList.java
@@ -79,7 +79,10 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this, ""+local.getName() , Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(FoodList.this, ""+local.getName() , Toast.LENGTH_SHORT).show();
+                         Intent foodDetail = new Intent(FoodList.this, FoodDetail.class);
+                         foodDetail.putExtra("FoodId", adapter.getRef(position).getKey());
+                         startActivity(foodDetail);
                     }
                 });
 
